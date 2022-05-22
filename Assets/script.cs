c27d80c1f996315f8e6fe7f2218d3d528c46981d
Natagrid <pup.natagrid@gmail.com>
2022-05-22 16:03:29 +0700

    chickenmove

--- Assets/script.cs
+++ Assets/script.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+    public float x, y, z;
+    Animator a;
     // Start is called before the first frame update
     void Start()
     {
-        
+        a = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(x, y, 0.02f);
+            a.Play("Walk In Place");
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(x, y, -0.02f);
+            a.Play("Walk In Place");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(x, -0.2f, z);
+            a.Play("Walk In Place");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(x, 0.1f, z);
+            a.Play("Walk In Place");
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(x, y, 0.05f);
+            a.Play("Run In Place");
+        }
+        else
+        {
+            transform.Translate(0, 0, 0);
+            a.Play("Idle");
+        }
     }
 }
